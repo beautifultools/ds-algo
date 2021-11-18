d166e8707fcbcc4a341717d41fa8fe8c1019e4e1
john <lord1229@gmail.com>
2021-11-18 23:45:01 +0900

    QueueByLinkedList 구현 시작

--- typescript/queue/QueueByLinkedList.ts
+++ typescript/queue/QueueByLinkedList.ts
@@ -1,11 +1,19 @@
-class SimpleQueue{
-    private arr: Array<number>;
+import SingleLinkedList from "../linkedlist/SingleLinkedList";
+
+class QueueByLinkedList{
+    private list: SingleLinkedList;
+
+    constructor(){
+        this.list = new SingleLinkedList();
+    }
 
     public enQueue(value:number):void {
-        this.arr.push(value)
+        this.list.insertInLinkedList(value, this.list.size);
     }
 
     public deQueue():number {
-        return this.arr.shift();
+        const node = this.list.getNode(0);
+        this.list.deletionOfNode(0);
+        return node.value;
     }
 }
